6559d584cd2c6aa1a873b00f5e0c4a9df1e41e5e
Chris Bowen <chrisbowen@comcast.net>
2022-10-04 18:08:28 -0500

    initial reverse numbers solution

--- reverse_number/reverse_number_solution.rb
+++ reverse_number/reverse_number_solution.rb
@@ -4,14 +4,22 @@
 # output: 
 
 # Steps
-# 1 - 
-# 2 -
-# 3 - 
+# 1 - Take in a number 
+# 2 - Put it in a string
+# 3 - Reverse the order
 
 # Commit 2 - Initial Solution
 
 def reverse_a_number(number)
-  # write code here  
+  number = number.to_s
+  reverse_number = ""
+  number.chars
+  array_length = number.length
+  number.length.times do 
+    reverse_number << number[array_length - 1]
+    array_length -= 1
+  end
+  return reverse_number.to_i
 end
 
 # Commit 3 - No Strings Solution
@@ -28,4 +36,10 @@ puts reverse_a_number(67352193) == 39125376
 puts reverse_a_number(97531) == 13579
 puts reverse_a_number(24) == 42
 puts reverse_a_number(1574362190881) == 1880912634751
+
+
+p reverse_a_number(67352193)  
+p reverse_a_number(97531)  
+p reverse_a_number(24)  
+p reverse_a_number(1574362190881)  
 
